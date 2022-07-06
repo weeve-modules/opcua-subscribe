401a4db63b50cd6ddb0e37bb1a15ba1f4cb54ccf
mesud pasic <839950+mesudpasic@users.noreply.github.com>
2022-07-06 10:51:30 +0200

    opc ua subscribe module initial build

---
+++ image/src/utils/util.js
@@ -0,0 +1,28 @@
+const { NESTED_RESPONSE, CUSTOM_FIELDS } = require('../config/config')
+const formatTimeDiff = (t1, t2) => {
+  const diff = Math.max(t1, t2) - Math.min(t1, t2)
+  const SEC = 1000,
+    MIN = 60 * SEC,
+    HRS = 60 * MIN
+  const hrs = Math.floor(diff / HRS)
+  const min = Math.floor((diff % HRS) / MIN).toLocaleString('en-US', { minimumIntegerDigits: 2 })
+  const sec = Math.floor((diff % MIN) / SEC).toLocaleString('en-US', { minimumIntegerDigits: 2 })
+  const ms = Math.floor(diff % SEC).toLocaleString('en-US', { minimumIntegerDigits: 4, useGrouping: false })
+  return `${hrs}:${min}:${sec}`
+}
+const isValidURL = str => {
+  var pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  ) // fragment locator
+  return !!pattern.test(str)
+}
+module.exports = {
+  formatTimeDiff,
+  isValidURL,
+}
